7afc2aa88836a0227f76e931301137959aacd45c
Nikita <nikita.jain@nagarro.com>
2021-04-17 21:55:56 +0530

    initial commit

---
+++ src/assignment00/Classs01.java
@@ -0,0 +1,14 @@
+package assignment00;
+
+import helper.MessageService;
+
+public class Classs01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("www");
+        MessageService messageService = new MessageService();
+        System.out.print(messageService.getMessage());
+    }
+
+}
